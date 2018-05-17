e07f059e9a0032cfa81641e60136234a2552cee0
Iktaniyalol <ilonellochka7@gmail.com>
2018-05-17 20:25:07 +1000

    делаю новую функцию

--- app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/Info.java
+++ app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/Info.java
@@ -1,13 +1,26 @@
 package iktaniyalol.ru.technicalassistant.interfaces;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import iktaniyalol.ru.technicalassistant.interfaces.R;
 
 public class Info extends MainActivity {
+    Button sys, resh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        sys = findViewById(R.id.sys);
+        resh = findViewById(R.id.resh4);
+        View.OnClickListener OLSys = new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(Info.this, Numbertosystem.class);
+                startActivity(intent);
+            }
+        };
+        sys.setOnClickListener(OLSys);
     }
 }
---
+++ app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/Numbertosystem.java
@@ -0,0 +1,14 @@
+package iktaniyalol.ru.technicalassistant.interfaces;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Numbertosystem extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbersys);
+    }
+}
