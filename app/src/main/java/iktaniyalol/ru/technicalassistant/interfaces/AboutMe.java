8438412b350baf743073907a1bcbf5639e7469e8
Iktaniyalol <ilonellochka7@gmail.com>
2018-05-28 00:53:37 +1000

    параболы готовы

---
+++ app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/AboutMe.java
@@ -0,0 +1,33 @@
+package iktaniyalol.ru.technicalassistant.interfaces;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.vansuita.materialabout.builder.AboutBuilder;
+import com.vansuita.materialabout.views.AboutView;
+
+public class AboutMe extends MainActivity {
+    ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        AboutView view = AboutBuilder.with(this)
+                .setPhoto(R.mipmap.profile_picture)
+                .setCover(R.mipmap.profile_cover)
+                .setName("Кисиленко Анастасия")
+                .setSubTitle("Разработчик приложения")
+                .setBrief("Собственный проект для IT Школы Samsung")
+                .setAppIcon(R.mipmap.ic_launcher)
+                .setAppName(R.string.app_name)
+                .addGitHubLink("user")
+                .addFacebookLink("user")
+                .setVersionNameAsAppSubTitle()
+                .setWrapScrollView(true)
+                .setLinksAnimated(true)
+                .setShowAsCard(true)
+                .build();
+
+        addContentView(view, lp);
+    }
+}
--- app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/MainActivity.java
+++ app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/MainActivity.java
@@ -1,23 +1,33 @@
 package iktaniyalol.ru.technicalassistant.interfaces;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import com.vansuita.materialabout.builder.AboutBuilder;
+import com.vansuita.materialabout.views.AboutView;
 
 
 public class MainActivity extends FragmentActivity {
 
     Button math, info;
-
+    ImageButton about;
+    ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         math = findViewById(R.id.math);
         info = findViewById(R.id.info);
-
+        about = findViewById(R.id.about);
 
         View.OnClickListener OLMath = new View.OnClickListener() {
             public void onClick(View view) {
@@ -31,9 +41,14 @@ public class MainActivity extends FragmentActivity {
                 startActivity(intent);
             }
         };
-
+        View.OnClickListener OLAbout = new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutMe.class);
+                startActivity(intent);
+            }
+        };
         math.setOnClickListener(OLMath);
         info.setOnClickListener(OLInfo);
-
+        about.setOnClickListener(OLAbout);
     }
 }
