b17614a7e821989e24b5d23cf71b9338c1c6f167
Iktaniyalol <ilonellochka7@gmail.com>
2018-05-31 18:54:21 +1000

    сохранение счета

--- app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/Auth.java
+++ app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/Auth.java
@@ -1,25 +1,41 @@
 package iktaniyalol.ru.technicalassistant.interfaces;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import com.google.android.gms.common.AccountPicker;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Auth extends MainActivity {
+    private EditText username;
+    private EditText password;
+    public Button login;
+    public static String nick = "test";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        View btn = (View) findViewById(R.id.sign_in_button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
+        username = (EditText) findViewById(R.id.edit_user);
+        password = (EditText) findViewById(R.id.edit_password);
+        login = (Button) findViewById(R.id.button_login);
+
+
+        View.OnClickListener OLLogin = new View.OnClickListener() {
             public void onClick(View view) {
 
-                Intent intent = AccountPicker.newChooseAccountIntent(null, null, new String[]{"com.google"},
-                        false, null, null, null, null);
-                startActivityForResult(intent, 123);
+                if (username.getText().toString().equals(nick) &&
+                        password.getText().toString().equals("test")) {
+                    Toast.makeText(getApplicationContext(), "Вход выполнен!", Toast.LENGTH_SHORT).show();
 
+                    Intent intent = new Intent(Auth.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Неправильные данные!", Toast.LENGTH_SHORT).show();
+                }
             }
-        });
+        };
+        login.setOnClickListener(OLLogin);
     }
 }
--- app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/Profile.java
+++ app/src/main/java/iktaniyalol/ru/technicalassistant/interfaces/Profile.java
@@ -5,14 +5,16 @@ import android.widget.TextView;
 
 public class Profile extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
-        TextView score1, score2, score3;
+        TextView score1, score2, score3, nick;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        nick = findViewById(R.id.nick);
         score1 = findViewById(R.id.score1);
         score2 = findViewById(R.id.score2);
         score3 = findViewById(R.id.score3);
         score1.setText(SchitGame.getScore(0));
         score2.setText(SchitGame.getScore(1));
         score3.setText(SchitGame.getScore(2));
+        nick.setText(Auth.nick);
     }
 }
